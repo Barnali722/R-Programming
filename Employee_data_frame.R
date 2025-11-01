96561d09191a5da93ab057e42f3000f67acd9e56
Barnali Tanti <tantibarnali520@gmail.com>
2025-11-01 21:41:31 +0530

    Add files via upload

---
+++ Employee_data_frame.R
@@ -0,0 +1,5 @@
+name<-c("Alice","Riya","Raghav","Barnali","Ram")
+Age<-c(25,30,21,42,25)
+Salary<-c(5000,6000,7000,7000,5000)
+employees<-data.frame(name,Age,Salary)
+employees
---
+++ Student_data_frame.R
@@ -0,0 +1,7 @@
+StudID<-c(001,002,003,004)
+Name<-c("Barnali","Riya","Alex","Maya")
+Contact<-c(12345678,641681568,619164281,678117630)   
+Address<-c("Konnagar","Murshidabad","Kolkata","Dum Dum")
+Student<-data.frame(StudID,Name, Contact, Address)
+Student
+
