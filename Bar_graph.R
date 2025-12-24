59a531a15663c7fef42b271a8e3f5645e22bc353
Barnali Tanti <tantibarnali520@gmail.com>
2025-12-24 21:19:43 +0530

    Add files via upload

---
+++ Bar_graph.R
@@ -0,0 +1,9 @@
+marks <- c(70, 85, 90, 75, 88)
+names <- c("A", "B", "C", "D", "E")
+
+barplot(marks,
+        names.arg = names,
+        col="skyblue",
+        main="Students Marks",
+        xlab="Students",
+        ylab="Marks")
